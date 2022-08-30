c37607aab3395eca0bfe061eee59900e6a504483
Pablo Opazo <107130554+PabloOpazo@users.noreply.github.com>
2022-08-30 12:26:22 -0400

    Update formulas.py

--- formulas.py
+++ formulas.py
@@ -8,13 +8,13 @@ while True:
     d = 0
     r = 0
     menu = input("1. Fórmula sumatoria aritmética\n2. Fórmula sumatoria geométrica\nsalir\n")
+    
     if menu=="1":
         # Fórmula sumatoria aritmética
         n = int(input("ingrese n: "))
         a = int(input("ingrese 1er termino: "))
         d = int(input("ingrese diferencia: "))
         formula = n/2 * (2*a + d*(n-1))
-
         resultado()
 
     elif menu=="2":
@@ -23,9 +23,7 @@ while True:
         a = int(input("ingrese 1er termino: "))
         r = int(input("ingrese razón: ")) 
         formula = (a * (r ** n)-1) / (r-1)
-
         resultado()
-        
 
     elif menu=="salir":
         break
